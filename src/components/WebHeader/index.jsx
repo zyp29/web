b09a8bf077dbfe3b389a597b13ff2a4c61445317
[zyp] <[1604434511@qq.com]>
2021-04-21 15:41:51 +0800

    新增

--- src/components/WebHeader/index.jsx
+++ src/components/WebHeader/index.jsx
@@ -1,10 +1,109 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Menu, Dropdown } from 'antd';
+import {
+    SettingFilled,
+    QuestionCircleOutlined,
+    CaretRightOutlined
+} from '@ant-design/icons';
 import headerLogo from '../../assets/headerLogo.png'
 import userhead from '../../assets/userhead .png'
 import './index.less'
 
+const menu = (
+    <div className="userSetting">
+        <div className="userInfoItem">
+            <img src={userhead} alt="" />
+            <div className="userDepartment">
+                <div>
+                    <Link>王青青</Link>
+                业务五部总经理
+                </div>
+                <div className="userAddress">
+                    泛微--上海大区/上海EBU七部.........
+                </div>
+            </div>
+        </div>
+        <div className="settingList">
+            <div className="settingItem">
+                <div className="settingTitle">
+                    <SettingFilled />
+                    <div className="textItem">
+                        字体设置
+                    </div>
+                </div>
+                <div className="settingAction">
+                    <div className="actionTitle">
+                        <QuestionCircleOutlined />
+                        <div style={{ padding: "0 10px" }}>
+                            默认
+                        </div>
+                        <CaretRightOutlined />
+                    </div>
+                </div>
+            </div>
+            <div className="settingItem">
+                <div className="settingTitle">
+                    <SettingFilled />
+                    <div className="textItem">
+                        语言选择
+                    </div>
+                </div>
+                <div className="settingAction">
+                    <div className="actionTitle">
+                        <div style={{ padding: "0 10px" }}>
+                            简体中文
+                        </div>
+                        <CaretRightOutlined />
+                    </div>
+                </div>
+            </div>
+            <div className="settingItem">
+                <div className="settingTitle">
+                    <SettingFilled />
+                    <div className="textItem">
+                        个性化设置
+                    </div>
+                </div>
+            </div>
+            <div className="settingItem">
+                <div className="settingTitle">
+                    <SettingFilled />
+                    <div className="textItem">
+                        密码设置
+                    </div>
+                </div>
+            </div>
+            <div className="settingItem">
+                <div className="settingTitle">
+                    <SettingFilled />
+                    <div className="textItem">
+                        主题中心
+                    </div>
+                </div>
+
+            </div>
+            <div className="settingItem">
+                <div className="settingTitle">
+                    <SettingFilled />
+                    <div className="textItem">
+                        布局选择
+                    </div>
+                </div>
+
+            </div>
+            <div className="settingItem">
+                <div className="settingTitle">
+                    <SettingFilled />
+                    <div className="textItem">
+                        退出
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+);
 const WebHeader = () => {
     return (
         <div>
@@ -57,13 +156,16 @@ const WebHeader = () => {
                             </Link>
                         </Menu.Item>
                     </Menu>
-                    <div className="userItem">
-                        <img src={userhead} alt="" />
-                        <div className="userInfo">
-                            <span>王青青</span>
-                            <span>运营主管</span>
+                    <Dropdown trigger="click" overlay={menu} placement="bottomLeft" arrow>
+                        <div className="userItem">
+                            <img src={userhead} alt="" />
+                            <div className="userInfo">
+                                <span>王青青</span>
+                                <span>运营主管</span>
+                            </div>
                         </div>
-                    </div>
+                    </Dropdown>
+
                     <div className="signOutBtn">
                         [退出]
             </div>
